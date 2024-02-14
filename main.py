6fdb5c80805e5c79f821b11a411f28c0accb8d36
Ivan Fioravanti <ivan.fioravanti@gmail.com>
2024-02-14 11:24:19 +0100

    Refactor performance test and add table format output
    The performance testing function was refactored to include a table output, presenting a clear comparison for data. The evaluation function now also tests multiple functions across various shapes, providing a more comprehensive test scope. PrettyTable was incorporated to provide a structured output of the test results.

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import time
 import mlx.core as mx
+import mlx.nn as nn
+from prettytable import PrettyTable
 
 
-def calculate_time(fun, x):
+def timeit(fun, x):
     # warm up
     for _ in range(10):
         mx.eval(fun(x))
-
     tic = time.perf_counter()
     for _ in range(100):
         mx.eval(fun(x))
     toc = time.perf_counter()
-
     return 1e3 * (toc - tic) / 100
 
 
-def timeit(fun, x):
-    iteration_time = calculate_time(fun, x)
-    print(f"Time per iteration {iteration_time:.3f} (ms)")
-    return iteration_time
+def evaluate_improvement(fun_name, fun, t, shape):
+    x = mx.random.uniform(shape=shape)
+    standard = timeit(fun, x)
+    t.add_row([fun_name, standard, "-"])
+    compiled = timeit(mx.compile(fun), x)
+    percentage_improvement = ((standard - compiled) / standard) * 100
+    t.add_row([f"compiled({fun_name})", compiled, f"{percentage_improvement:00.1f}%"])
+    return t
 
 
-x = mx.random.uniform(shape=(32, 1000, 4096))
-standard = timeit(mx.softmax, x)
-compiled = timeit(mx.compile(mx.softmax), x)
-percentage_improvement = ((standard - compiled) / standard) * 100
+table = PrettyTable(["Function", "Duration", "Improvement"])
+table.align["Function"] = "l"
+table.align["Duration"] = "r"
+table.align["Improvement"] = "r"
+table.float_format = ".3"
+
+shape = (32, 1000, 4096)
+table = evaluate_improvement("mx.softmax", mx.softmax, table, shape)
+
+shape = (128, 16, 1024)
+table = evaluate_improvement("nn.relu", nn.relu, table, shape)
+
+shape = (64, 128, 1024)
+table = evaluate_improvement("nn.softplus", nn.softplus, table, shape)
+
+shape = (64, 128, 1024)
+table = evaluate_improvement("nn.log_sigmoid", nn.log_sigmoid, table, shape)
+
+print(table)
